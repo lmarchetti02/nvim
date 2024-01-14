cda6143ad74f198d5f1022df20e9fdd07a1d9182
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-14 23:53:57 +0100

    added cpplint

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,6 +10,7 @@ return {
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.beautysh,
 				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.diagnostics.cpplint,
 			},
 		})
 
