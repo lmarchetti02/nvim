0759805f61524c82dbfa15c37901dfd03486ba2c
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-14 23:08:50 +0100

    added none-ls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
 -- TREESITTER
 
 return {
-	"nvim-treesitter/nvim-treesitter", 
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
+			auto_install = true,
 			ensure_installed = { "c", "cpp", "lua", "cmake", "python", "latex", "csv" },
 			higlight = { enable = true },
-			indent = { enable = true }
+			indent = { enable = true },
 		})
-	end
+	end,
 }
