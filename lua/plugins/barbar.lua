a6457bc6c59ae0ba45f166e64ec2d18f0d511d7d
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-16 23:05:34 +0100

    added which-key

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -16,6 +16,7 @@ return {
 		animation = true,
 		sidebar_filetypes = {
 			["neo-tree"] = { event = "BufWipeout" },
+			["telescope"] = { event = "BufWipeout" },
 		},
 	},
 	version = "^1.0.0", -- optional: only update when a new 1.x version is released
