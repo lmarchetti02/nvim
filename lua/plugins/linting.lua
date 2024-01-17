805d859c44180eedd015b272b7c208f733bb78b0
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-17 17:00:28 +0100

    Modified c++ linter

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -6,7 +6,7 @@ return {
 
 		lint.linters_by_ft = {
 			python = { "pylint" },
-			cpp = { "cppcheck" },
+			cpp = { "clangtidy" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
