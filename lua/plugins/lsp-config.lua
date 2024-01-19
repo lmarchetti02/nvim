fa4b71601a8caecd867db353339e461495c5414d
lmarchetti02 <00lucamarchetti00@gmail.com>
2024-01-19 16:52:08 +0100

    Added snippets and autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,14 +1,28 @@
 return {
 	"neovim/nvim-lspconfig",
+	dependencies = { "hrsh7th/cmp-nvim-lsp" },
 	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local lspconfig = require("lspconfig")
 
-		lspconfig.lua_ls.setup({})
-		lspconfig.clangd.setup({})
-		lspconfig.cmake.setup({})
-		lspconfig.julials.setup({})
-		lspconfig.marksman.setup({})
-		lspconfig.pyre.setup({})
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.cmake.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.julials.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.marksman.setup({
+			capabilities = capabilities,
+		})
+		lspconfig.pyre.setup({
+			capabilities = capabilities,
+		})
 
 		vim.keymap.set("n", "H", vim.lsp.buf.hover, {}) -- get documentation
 		vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, {}) -- go to definition
