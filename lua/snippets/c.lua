b1ebb97026afc02fcdbc32cea5f1b9b3588a0033
Luca Marchetti <90146345+lmarchetti02@users.noreply.github.com>
2025-11-01 23:58:07 +0100

    added c snippets

---
+++ lua/snippets/c.lua
@@ -0,0 +1,44 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+
+return {
+	-- Main function
+	s("main", {
+		t({ "int main(int argc, char *argv[]) {", "    " }),
+		i(1, "// code here"),
+		t({ "", "    return 0;", "}" }),
+	}),
+
+	-- Include guard
+	s("header", {
+		t("#ifndef "),
+		i(1, "HEADER_H"),
+		t({ "", "#define " }),
+		f(function(args)
+			return args[1][1]
+		end, { 1 }),
+		t({ "", "", "" }),
+		i(2, "// declarations"),
+		t({ "", "", "#endif // " }),
+		f(function(args)
+			return args[1][1]
+		end, { 1 }),
+	}),
+
+	-- Switch statement
+	s("switch", {
+		t("switch ("),
+		i(1, "var"),
+		t({ ") {", "    case " }),
+		i(2, "value"),
+		t({ ":", "        " }),
+		i(3, "break;"),
+		t({ "", "    default:", "        " }),
+		i(4, "break;"),
+		t({ "", "}" }),
+	}),
+}
--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -2,3 +2,4 @@ local ls = require("luasnip")
 
 -- Load Python snippets
 ls.add_snippets("python", require("snippets.python"))
+ls.add_snippets("c", require("snippets.c"))
