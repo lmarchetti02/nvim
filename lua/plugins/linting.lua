8b8b4bdd3371b220467586d4713e9e4fe2afe500
Luca <luca.marchetti@tum.de>
2026-07-16 13:12:27 +0200

    minor bugfix

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -12,9 +12,17 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+		-- clang-tidy (unlike clangd) can't infer compile flags for a header from whichever
+		-- .cpp includes it, so it fails on standalone headers even when clangd handles them
+		-- fine; skip linting on headers and leave those diagnostics to clangd.
+		local header_extensions = { h = true, hh = true, hpp = true, hxx = true }
+
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
+				if header_extensions[vim.fn.expand("%:e")] then
+					return
+				end
 				lint.try_lint()
 			end,
 		})
