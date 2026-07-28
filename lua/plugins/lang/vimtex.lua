641a9a1147e59bee48e64df1b0e7a32a4457150d
Luca Marchetti <90146345+lmarchetti02@users.noreply.github.com>
2026-07-28 16:44:57 +0200

    Move LaTeX-specific autocmds/keymap from vim-options.lua into vimtex.lua

--- lua/plugins/lang/vimtex.lua
+++ lua/plugins/lang/vimtex.lua
@@ -8,4 +8,32 @@ return {
 		-- Don't open QuickFix for compilation warnings, only errors
 		vim.g.vimtex_quickfix_open_on_warning = 0
 	end,
+	config = function()
+		-- Automatically format multiple empty lines in LaTeX on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*.tex",
+			callback = function()
+				-- Save the current cursor position so it doesn't jump around
+				local save_cursor = vim.fn.getpos(".")
+
+				-- Silently search and replace 3 or more newlines with exactly 2
+				-- The 'e' flag prevents error messages if no extra lines are found
+				vim.cmd([[%s/\n\{3,}/\r\r/ge]])
+
+				-- Restore cursor position
+				vim.fn.setpos(".", save_cursor)
+			end,
+		})
+
+		-- LaTeX shortcuts
+		vim.keymap.set("n", "<leader>dl", "<leader>wj:q<CR>", { remap = true, desc = "Close LaTeX compilation error" })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "tex",
+			callback = function()
+				-- <M-M> represents Alt/Option + Shift + M
+				-- It inserts $ $, then moves the cursor left one space to put you inside
+				vim.keymap.set("i", "<M-M>", "$  $<Left><Left>", { buffer = true, desc = "Insert inline math" })
+			end,
+		})
+	end,
 }
